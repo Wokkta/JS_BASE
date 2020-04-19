af844393185b04fa4a72ad6d789fd5177f020595
Wokta <55146833+Wokkta@users.noreply.github.com>
2020-04-19 19:42:45 +0300

    test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+let gold = parseInt(prompt('gold'))
+let apple = parseInt(prompt('apple'))
+let bread = parseInt(prompt('bread'))
+let applecost = parseInt(prompt('cost of apple'))
+let breadcost = parseInt(prompt('cost of bread'))
+(apple*applecost+bread*breadcost)<=gold
