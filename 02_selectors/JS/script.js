61d1ff4c7f7d82d6a7f2bf4211a8a11ef4a7c2e0
Wokta <awerty17@mail.ru>
2020-04-29 16:11:08 +0300

    k

---
+++ 02_selectors/JS/script.js
@@ -0,0 +1,10 @@
+const byTag = document.getElementsByTagName('div');
+const byClass = document.getElementsByClassName('myne');
+const byId = document.getElementById('myid');
+console.log(byTag,byClass,byId);
+const allBySelector = document.querySelectorAll('.coolClass.mine');
+const firstBySelector = document.querySelector('.coolClass.mine');
+console.log(allBySelector,firstBySelector);
+byId.innerHTML = "<h2>Hello i am a div</h2>"
+firstBySelector.innerHTML = "<h2>Hello i am firstBySelector</h2>"
+const quest = prompt("Цто хотите написать на странице ?");
