71b00645c336955ca8e627015ac95e6bc7d20118
Wokta <awerty17@mail.ru>
2020-05-04 10:12:22 +0300

    d

---
+++ practice_3/JS/script.js
@@ -0,0 +1,14 @@
+ const background = prompt('Какой будет фон у страницы?');
+  const text_color = prompt('Какой будет цвет текста на странице');
+   const person = prompt('Как зовут человека, который вас вдохновляет');
+    const img_adress = prompt('Введите адрес картинки');
+     const text = prompt('Введите текст страницы');
+     document.body.style.backgroundColor = "background";
+     document.body.style.color = "text_color";
+     const person_div = document.getElementsByClassName('name');
+     person_div.innerHTML = "<h2>person</h2>";
+
+     const img_div = document.getElementsByClassName('imageBlock');
+     img_div.style.backgroundImage = 'img_adress';
+     const shortBio = document.getElementsByClassName('shortBio');
+     shortBio.innerHTML = '<h1>text</h1>';
