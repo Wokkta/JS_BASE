5c9f9b46518c8368e827835bbfc3a90a2aa543cd
Wokta <awerty17@mail.ru>
2020-05-16 14:44:10 +0300

    d

--- practice_3/JS/script.js
+++ practice_3/JS/script.js
@@ -1,10 +1,14 @@
- const background = prompt('Какой будет фон у страницы?');
- const text_color = prompt('Какой будет цвет текста на странице');
- const person = prompt('Как зовут человека, который вас вдохновляет');
- const img_adress = prompt('Введите адрес картинки');
- const text = prompt('Введите текст страницы');
- const page = document.getElementsByClassName('page');
-const shortBio = document.getElementsByClassName('shortBio');
+ const background = prompt('Какой будет фон у страницы?')
+ const text_color = prompt('Какой будет цвет текста на странице')
+ const person = prompt('Как зовут человека, который вас вдохновляет')
+ const img_adress = prompt('Введите адрес картинки')
+ const text = prompt('Введите текст страницы')
+ const page = document.getElementsByClassName('page')
+const shortBio = document.querySelector('.shortBio')
 document.body.style.backgroundColor = background
 shortBio.innerHTML =text
-document.body.style.backgroundColor = text_color;
+document.body.style.color = text_color
+const image = getElementsByTagName('img')
+image.setAttribute('src',img_adress)
+const person_div = querySelector('.name')
+person_div.innerHTML = person
