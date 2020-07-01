f2e8c549edffcc1be1b6275fbbccc1804189cfaf
Wokta <awerty17@mail.ru>
2020-07-01 11:22:18 +0300

    а

---
+++ functions/JS/script.js
@@ -0,0 +1,5 @@
+function Hello(name){
+	return 'Hello ' + y
+}
+let y = prompt('name')
+document.body.innerHTML = (Hello(y))
---
+++ switch/JS/script.js
@@ -0,0 +1,12 @@
+let answer = prompt('what?');
+answer = answer.toLowerCase();
+switch(answer){
+	case "dr. dre":
+	console.log('rap');
+	break;
+	case "zimmer":
+	console.log('classic');
+	break;
+	default:
+	console.log('not found')
+}
