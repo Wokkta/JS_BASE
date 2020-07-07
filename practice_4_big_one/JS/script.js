a94bc0b5e1998827462417f7fa35261e1a01593b
Wokta <awerty17@mail.ru>
2020-07-07 20:09:44 +0300

    s

--- practice_4_big_one/JS/script.js
+++ practice_4_big_one/JS/script.js
@@ -1,8 +1,26 @@
-const button = document.querySelector('.popup');
-const x = document.querySelector('.fas fa-times');
-button.onclick = ()=>{
-  button.style.display= 'flex';
+const button = document.querySelector('button');
+var state = 'flex';
+const x = document.querySelector('.closePopup');
+const x_for_burger = document.querySelector('.closeMenu');
+const burger = document.querySelector('.openMenu');
+button.onclick =()=>{
+  if (state=='flex'){
+    document.querySelector('.popup').style.display = 'none';
+    state = 'none';
+  }
+  else if (state =="none"){
+    document.querySelector('.popup').style.display = 'flex';
+    state='flex';
+  }
 }
 x.onclick = ()=>{
-  button.style.display = 'none';
+  document.querySelector('.popup').style.display = 'none';
+  state = 'none';
 }
+burger.onclick = ()=>{
+  document.querySelector('.menu').style.left = 0;
+}
+x_for_burger.onclick = ()=>{
+  document.querySelector('.menu').style.left = '-50vw';
+}
+/// at codepen works normal
