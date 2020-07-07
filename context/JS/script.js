f76ada08a6944e13dfbc38545ebdae8651aa4db8
Wokta <awerty17@mail.ru>
2020-07-07 17:19:12 +0300

    d

---
+++ context/JS/script.js
@@ -0,0 +1,12 @@
+console.log('hi '+ this);
+const button  = document.querySelector('button');
+button.onclick = function(){
+	console.log('button '+this);
+	this.style.color = 'orange';
+	this.style.width =this.style.width*10;
+	this.style.height  = this.style.height*10;
+}
+window.addEventListener('load',()=>{
+	console.log('window '+ this);
+})
+///при стрелочной функции this всегда равен window
