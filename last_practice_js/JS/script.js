a937bbf2ffc6b2bc4785022b790429ffdde4b8de
Wokta <awerty17@mail.ru>
2020-08-24 20:31:21 +0300

    g

--- last_practice_js/JS/script.js
+++ last_practice_js/JS/script.js
@@ -1,88 +1,59 @@
-const APIKey ='cc0fdd4b8c2f8c50d12262eed669fcf4'
-
-
-
-
-
-
-
-
-
-
 
 var icon = document.querySelector('.icon');
-const city = 'Москва'
-const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey+'';
-	let xhr = new XMLHttpRequest();
-	if( xhr.status !==200){
-	console.log(xhr.status);
-console.log(xhr.statusText);
-}
-else{
-let DATA = JSON.parse(xhr.responseText);
-console.log(DATA);
-console.log('hi');
-let Temp = DATA.main.temp - 273;
-if(Temp > 15) {
-				return this.happiness = this.happiness+1
-			}else {
-				return this.happiness
-			}
+window.addEventListener('load', function() {
+	class Person {
+		constructor(name){
+			this.name = name;
+			this.happiness = 0;
+		}
+		hasCat(){return this.happiness = this.happiness+1}
+		hasRest(){return this.happiness = this.happiness+1}
+		hasMoney(){return this.happiness = this.happiness+1}
+		isSunny() {
+				const APIKey ='cc0fdd4b8c2f8c50d12262eed669fcf4';
+				const city = 'Москва';
+				const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
 
-}
-class Person {
-	constructor(name){
-		this.name = name
-		this.happiness = 0
-	}
-	hasCat(){
-		this.happiness +=1
+				let xhr = new XMLHttpRequest();
 
-	}
-	hasRest(){
-		this.happiness +=1
-	}
-	hasMoney(){
-		this.happiness +=1
-	}
-	isSunny(){
-		//получает данные о погоде в Москве из API и в случае если температура в цельсиях больше 15 градусов. увеличивает this.happiness на 1 и возвращает this.happiness (всегда возвращается this.happiness, независимо, увеличили на 1 или нет).
-	}
-}
-const form = document.querySelector('form');
+				setTimeout(xhr.open('GET', url, false), 0);
+
+				xhr.send();
+
+				let DATA = JSON.parse(xhr.responseText);
+				let Temp = DATA.main.temp - 273;
+
+				if(Temp > 15) {
+					return this.happiness = this.happiness+1
+				}else {
+					return this.happiness
+				}
+}}
+const form = document.forms[0];
 form.onsubmit = function(e){
 	e.preventDefault();
 	// everything is ok there
 	var Human = new Person(form.elements.name.value);
-	document.querySelector('.personName').innerHtml = Human.name
-	console.log('here')
+	let userName = document.querySelector('.personName')
+	let icon = document.querySelector('.icon')
+	userName.innerHTML = form.elements.name.value;
 
-	if(form.elements.cat.value == 'yes'){
-		Human.hasCat()
-		// everything is ok there
-	}
-	if (form.elements.rest.value == 'yes'){
-		Human.hasRest()
-		// everything is ok there
-	}
-	if (form.elements.money.value =='yes'){
-		Human.hasMoney()
-		// everything is ok there
-	}
-
+	if(form.elements.cat.value == 'yes'){Human.hasCat()}
+	if (form.elements.rest.value == 'yes'){Human.hasRest()}
+	if (form.elements.money.value =='yes'){Human.hasMoney()}
+	Human.isSunny()
 
 	if (Human.happiness == 4){
-	icon.innerHtml = '&#128513'
+	icon.innerHtml = '&#128513';
 }
-else {
-	// everything is ok there
-	if(Human.happiness ==3 || Human.happiness ==2){
-		icon.innerHtml ='&#128528'
+else if(Human.happiness ==3 || Human.happiness ==2){
+		icon.innerHtml ='&#128528';
 	}
 	else{
 		// everything is ok there
-		icon.innerHtml ='&#128577'
+		icon.innerHtml ='&#128577';
 	}
-}
+
 	
 }
+})
